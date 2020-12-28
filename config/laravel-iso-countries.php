b3d821818e17e78e2e39503241d16c80f9a3be42
Martin <martin.migge@gmail.com>
2020-12-28 19:05:03 +0100

    Added default config

--- config/laravel-iso-countries.php
+++ config/laravel-iso-countries.php
@@ -2,4 +2,13 @@
 
 return [
 
+    // Supported locales for names (countries, languages, currencies)
+    // IMPORTANT: After modifying, (re-)seed the database to apply changes
+    'locales' => [
+        'en',
+        'de',
+        'fr',
+        'es',
+    ],
+
 ];
